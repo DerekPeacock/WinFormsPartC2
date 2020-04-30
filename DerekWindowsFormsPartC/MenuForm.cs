6c276d93e60e49270f288bd1793f3f1c8ade3248
Derek Peacock <derek.peacock@yahoo.com>
2020-04-30 14:10:46 +0100

    Initial commit

---
+++ DerekWindowsFormsPartC/MenuForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsFormsPartC.Unit5;
+
+namespace WindowsFormsPartC
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void runProgram(object sender, EventArgs e)
+        {
+            if(animationRadioButton.Checked)
+            {
+                // Tasks 5.1 and 5.2
+
+                AnimationForm form = new AnimationForm();
+                form.Show();
+            }
+            else if(flyMeRadioButton.Checked)
+            {
+                // Tasks 5.3, 5.4 and 5.5
+                FlyMeForm form = new FlyMeForm();
+                form.Show();
+            }
+        }
+
+        private void quitForm(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ DerekWindowsFormsPartC/Unit5/FlyMeForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsPartC.Unit5
+{
+    /// <summary>
+    /// This form shows an animation of a helicopter which will fly across
+    /// and down the screen.
+    /// </summary>
+    public partial class FlyMeForm : Form
+    {
+        private Animation animation = new Animation(4);
+
+        public FlyMeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void quitForm(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void startStopAnimation(object sender, EventArgs e)
+        {
+            if(stopRadioButton.Checked)
+            {
+                animationTimer.Enabled = false;
+            }
+            else
+            {
+                animationTimer.Enabled = true;
+            }
+        }
+
+        private void updateCopter(object sender, EventArgs e)
+        {
+            copterPictureBox.Image = animation.GetNextImage();
+        }
+
+        private void loadImages(object sender, EventArgs e)
+        {
+            string baseFileName = "../../Images/Copter/copter";
+            animation.LoadImages(baseFileName);
+        }
+    }
+}
